bdc4c7744aab897fd0035680257a75e9dbdc8460
Gesa Siebert <hallo@gesasiebert.de>
2022-10-19 09:06:19 +0200

    delete styling of count

--- components/Card/index.js
+++ components/Card/index.js
@@ -9,9 +9,7 @@ export default function Card({ date, time, nistkasten, count }) {
         {date}, {time}
       </SecondaryInfo>
 
-      <p>
-        <StyledCount>{count}</StyledCount> Fledermäuse
-      </p>
+      <p>{count} Fledermäuse</p>
     </StyledCard>
   );
 }
@@ -30,16 +28,4 @@ const StyledCard = styled.li`
 const SecondaryInfo = styled.p`
   font-size: 0.75rem;
   color: var(--primary-gray);
-`;
-
-const StyledCount = styled.span`
-  background-color: var(--primary-white);
-  padding: 0.5rem;
-  border: 0.5rem;
-  border-style: solid;
-  border-color: var(--primary-black);
-  border-radius: 50%;
-  display: inline-block;
-  min-width: 54px;
-  text-align: center;
 `;

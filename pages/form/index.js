c6796fbc3d9f07ac133c183cfe29fff88d4150ac
Gesa Siebert <hallo@gesasiebert.de>
2022-10-19 17:28:16 +0200

    set up input fields

--- pages/form/index.js
+++ pages/form/index.js
@@ -3,13 +3,40 @@ import styled from "styled-components";
 import Link from "next/link";
 import React from "react";
 
-export default function index() {
+export default function Create() {
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const { date, time, latitude, longitude, boxnumber, count } =
+      Object.fromEntries(formData);
+  }
+
   return (
     <>
-      <div>test</div>
+      <StyledForm onSubmit={handleSubmit}>
+        <label htmlFor="date">Date</label>
+        <input type="date" name="date" id="date" required></input>
+        <label htmlFor="time">Time</label>
+        <input type="time" name="time" id="time" required></input>
+        <label htmlFor="count">Anzahl Fledermäuse</label>
+        <input type="number" name="count" id="count" required></input>
+        <label htmlFor="latitude">Geografische Breite</label>
+        <input type="number" name="latitude" id="latitude" required></input>
+        <label htmlFor="longitude">Geografische Länge</label>
+        <input type="number" name="longitude" id="longitude" required></input>
+        <label htmlFor="boxnumber">Nistkasten Nr.</label>
+        <input type="number" name="boxnumber" id="boxnumber" required></input>
+      </StyledForm>
       <Link href="/">
         <StyledAnchor>Abbrechen</StyledAnchor>
       </Link>
     </>
   );
 }
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+`;

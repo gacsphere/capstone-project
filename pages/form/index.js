29fa4274498daf0e132ccefb958621d5f8e1e769
Gesa Siebert <hallo@gesasiebert.de>
2022-10-19 15:28:20 +0200

    add abbrechen-button in form leading to index.js

--- pages/form/index.js
+++ pages/form/index.js
@@ -1,5 +1,26 @@
+import styled from "styled-components";
+import Link from "next/link";
 import React from "react";
 
 export default function index() {
-  return <div>test</div>;
+  return (
+    <>
+      <div>test</div>
+      <Link href="/">
+        <AddBoxAnchor>Abbrechen</AddBoxAnchor>
+      </Link>
+    </>
+  );
 }
+
+const AddBoxAnchor = styled.a`
+  background-color: var(--primary-black);
+  color: var(--primary-white);
+  display: flex;
+  justify-content: center;
+  padding: 1rem;
+  :hover {
+    background-color: var(--primary-gray);
+    cursor: pointer;
+  }
+`;

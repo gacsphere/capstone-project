81102d91aadcdf5c52fd77980d81e0aa3b3f21b7
Gesa Siebert <hallo@gesasiebert.de>
2022-10-20 10:34:52 +0200

    add basic styling to submit-button

--- components/Form/index.js
+++ components/Form/index.js
@@ -12,6 +12,8 @@ export default function Create({ onCreate }) {
       Object.fromEntries(formData);
 
     onCreate(date, time, latitude, longitude, boxnumber, count);
+    event.target.reset();
+    event.target.elements.question.focus();
   }
   //   const today = new Date().toISOString().slice(0, 10);
   //   const todayTime = new Date();
@@ -32,16 +34,29 @@ export default function Create({ onCreate }) {
         <input type="number" name="longitude" id="longitude"></input>
         <label htmlFor="boxnumber">Nistkasten Nr.</label>
         <input type="number" name="boxnumber" id="boxnumber"></input>
+        <StyledButton type="submit">Speichern</StyledButton>
       </StyledForm>
-      <button type="submit" onSubmit={handleSubmit}>
-        Create new Card
-      </button>
+
       {/* <Link href="/">
         <StyledAnchor>Abbrechen</StyledAnchor>
       </Link> */}
     </>
   );
 }
+
+const StyledButton = styled.button`
+  background-color: var(--primary-black);
+  color: var(--primary-white);
+  display: flex;
+  justify-content: center;
+  padding: 1rem;
+  border: none;
+  margin-top: 1rem;
+  :hover {
+    background-color: var(--primary-gray);
+    cursor: pointer;
+  }
+`;
 
 const StyledForm = styled.form`
   display: flex;
